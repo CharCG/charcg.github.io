7f0a9c39404da58233a6a10a5506915895d0a28e
Charles <charlescong91@gmail.com>
2022-04-12 16:13:13 +0700

    feat: type effect

---
+++ js/index.js
@@ -0,0 +1,13 @@
+try {
+    new Typed(".typed", {
+        strings: [
+            "CharCG.",
+            "Charles."
+        ],
+        typeSpeed: 75,
+        backSpeed: 50,
+        loop: true
+    });
+} catch(error) {
+    console.log(error);
+};
